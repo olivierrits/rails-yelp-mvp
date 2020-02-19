9350e372e36bce3960fe4e49c46376199c9b38a1
Olivier Rits <olivier.rits@imec.be>
2020-02-19 16:35:22 +0100

    working version with for now poor front end

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
+  STARS = [0, 1, 2, 3, 4, 5]
   belongs_to :restaurant
   validates :content, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: STARS }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-  get 'restaurants/new'
-  get 'restaurants/create'
-  get 'restaurants/show'
+  get 'reviews/new'
+  get 'reviews/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:new, :create, :index, :show]
+  resources :restaurants, only: [:new, :create, :index, :show] do
+    resources :reviews, only: [ :new, :create ]
+  end
+
   root to: 'restaurants#index'
 end
